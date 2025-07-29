27e198175676b0722fa8fbe516b1055791d80240
GennyCryPotent <153099957+GennyCryPotent@users.noreply.github.com>
2025-07-29 13:22:16 +0200

    Aggiunta componenti map

---
+++ streetcats-frontend/src/app/shared/map-display/map-display.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-display',
+  imports: [],
+  templateUrl: './map-display.html',
+  styleUrl: './map-display.scss'
+})
+export class MapDisplay {
+
+}
