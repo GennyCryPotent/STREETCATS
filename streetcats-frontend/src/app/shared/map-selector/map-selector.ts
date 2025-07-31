5b23dd27aaf629053d51f316c8cddb2a504e5961
GennyCryPotent <153099957+GennyCryPotent@users.noreply.github.com>
2025-08-01 00:38:15 +0200

    Test

---
+++ streetcats-frontend/src/app/shared/map-selector/map-selector.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-selector',
+  imports: [],
+  templateUrl: './map-selector.html',
+  styleUrl: './map-selector.scss'
+})
+export class MapSelector {
+
+}
